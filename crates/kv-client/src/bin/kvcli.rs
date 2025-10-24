bc995c12cd8388abbe6924448af7c048cb55787b
eemmtt <etmuzio@gmail.com>
2025-10-24 11:49:24 -0400

    comments

--- crates/kv-client/src/bin/kvcli.rs
+++ crates/kv-client/src/bin/kvcli.rs
@@ -4,27 +4,29 @@ use std::str::from_utf8;
 use kv_client::{kvc_connect, kvc_get, kvc_set, kvc_delete };
 
 fn main() {
+    
+    // todo get cli args, parse them...
+    
     println!("client: start");
-
-    /* get cli args, parse them... */
     let sock_addr = UnixAddr::new("./kv.sock").unwrap();
-
     let connection = kvc_connect(sock_addr).unwrap();
 
+    /* try GET */
     let mut val = kvc_get(&connection, "test").unwrap();
     let mut msg_got = from_utf8(&val.data[..val.size]).expect("invalid utf-8");
     println!("client: got '{}' from get()", msg_got);
 
+    /* try SET */
     val = kvc_set(&connection, "test", "oogabooga").unwrap();
     msg_got = from_utf8(&val.data[..val.size]).expect("invalid utf-8");
     println!("client: got '{}' from set()", msg_got);
 
+    /* try DEL */
     val = kvc_delete(&connection, "test").unwrap();
     msg_got = from_utf8(&val.data[..val.size]).expect("invalid utf-8");
     println!("client: got '{}' from del()", msg_got);
 
     close(connection.fd).expect("close sockfd failed");
-
     println!("client: stop");
 
 }

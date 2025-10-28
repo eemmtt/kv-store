ffdb61107b379f04287d0c5ef6e7c5246881a0de
eemmtt <etmuzio@gmail.com>
2025-10-28 13:00:39 -0400

    add polling to server loop, move accept() logic into handle_connection()

--- crates/kv-server/src/lib.rs
+++ crates/kv-server/src/lib.rs
@@ -1,5 +1,6 @@
-use std::{os::fd::{AsRawFd, OwnedFd}, path::Path};
-use nix::{errno::Errno, sys::socket::{bind, listen, socket, AddressFamily, Backlog, SockFlag, SockType, UnixAddr}, unistd::unlink};
+use std::{os::fd::{RawFd, AsRawFd, OwnedFd, FromRawFd}, path::Path};
+use nix::{errno::Errno, sys::socket::{accept,bind, listen, socket, AddressFamily, Backlog, SockFlag, SockType, UnixAddr}, unistd::unlink};
+use kv_shared::{send_all, recv_all, KVConnection, KVValue};
 
 /// Get value from log
 pub fn log_get(){}
@@ -34,4 +35,68 @@ pub fn open_socket(path: &Path) -> Result<OwnedFd, Errno>{
     listen(&sockfd, Backlog::MAXCONN).expect("open_socket: listen failed");
 
     Ok(sockfd)
+}
+
+pub enum PollInterests {
+    ListeningSocket = 0,
+    TerminalInput = 1,
+    Signal = 2,
+}
+
+pub fn handle_connection(socket_fd: &OwnedFd) -> Result<(), Errno>{
+    let connfd_raw: RawFd = accept(socket_fd.as_raw_fd()).expect("accept failed");
+    let connfd = unsafe { OwnedFd::from_raw_fd(connfd_raw) };
+    println!("server: connection accepted");
+
+    let connection = KVConnection{
+        fd: connfd,
+        mtu: 1024,
+    };
+
+    loop {
+
+        let result = match recv_all(&connection){
+            Ok(kvv) => kvv,
+            Err(Errno::ECONNRESET) => {
+                println!("handle_connection: client disconnected");
+                break;
+            },
+            Err(e) => {
+                eprintln!("kv-server: kv-shared::recv_all: error {}", e);
+                return Err(e);
+            }
+        };
+    
+        let result_as_str = String::from_utf8(result.data).unwrap();
+        let mut parts = result_as_str.trim().splitn(3, ' ');
+    
+        let command = parts.next().unwrap_or("");
+        let key = parts.next().unwrap_or("");
+        let value = parts.next().unwrap_or("");
+    
+        match command {
+            "GET" => {
+                let msg = String::from("good get!").into_bytes();
+                send_all(&connection, msg).unwrap();
+                println!("handle_connection: handled GET");
+            },
+            "SET" => {
+                let msg = String::from("good set!").into_bytes();
+                send_all(&connection, msg).unwrap();
+                println!("handle_connection: handled SET");
+    
+            },
+            "DEL" => {
+                let msg = String::from("good del!").into_bytes();
+                send_all(&connection, msg).unwrap();
+                println!("handle_connection: handled DEL");
+    
+            },
+            _ => {
+                println!("handle_connection: received unknown command {}", result_as_str);
+            }
+        }
+    }
+
+    Ok(())
 }
--- crates/kv-server/src/main.rs
+++ crates/kv-server/src/main.rs
@@ -1,14 +1,18 @@
-use kv_shared::{recv_all, send_all, KVConnection};
 use nix::errno::Errno;
-use nix::sys::socket::{accept};
+use nix::poll::PollTimeout;
+use nix::sys::epoll::{Epoll, EpollCreateFlags, EpollEvent, EpollFlags};
 use nix::unistd::{close, unlink};
-use std::os::fd::{AsRawFd, FromRawFd, OwnedFd, RawFd};
+use std::collections::HashMap;
+use std::os::fd::{ OwnedFd};
 use std::path::Path;
 
-use kv_server::{self, open_socket};
+use kv_server::{self, PollInterests, handle_connection, open_socket};
 
 fn main() -> Result<(), Errno> {
     println!("server: start");
+
+    /* todo: init prethreading */
+
 
     let socket_path = Path::new("./kv.sock");
     let socket_fd = match open_socket(socket_path){
@@ -18,66 +22,47 @@ fn main() -> Result<(), Errno> {
             return Err(e);
         }
     };
-    
-    println!("server: listening on socket");
-    'accept_connections: loop {
 
-        // add polling
-        
-        let connfd_raw: RawFd = accept(socket_fd.as_raw_fd()).expect("accept failed");
-        let connfd = unsafe { OwnedFd::from_raw_fd(connfd_raw) };
-        println!("server: connection accepted");
+    /* register PollInterests to fds */
+    let mut fds: HashMap<u64, &OwnedFd> = HashMap::new();
+    fds.insert(PollInterests::ListeningSocket as u64, &socket_fd);
 
-        let connection = KVConnection{
-            fd: connfd,
-            mtu: 1024,
+    /* add interests to epoll */
+    let epoll = Epoll::new(EpollCreateFlags::empty())?;
+    match epoll.add(&socket_fd, EpollEvent::new(EpollFlags::EPOLLIN | EpollFlags::EPOLLET, PollInterests::ListeningSocket as u64)){
+        Ok(_) => (),
+        Err(e) => {
+            eprintln!("server: epoll.add() {}", e);
+            return Err(e);
+        }
+    };
+
+    /* todo: accept commands from stdin */
+    /* todo: handle signals gracefully with signalfd */
+
+    /* start polling */
+    let mut events = [EpollEvent::empty()];
+    let mut poll_results_num: usize = 0;
+    'polling: loop {
+        println!("server: polling");
+        poll_results_num = match epoll.wait(&mut events, PollTimeout::NONE){
+            Ok(size) => size,
+            Err(e) => {
+                eprintln!("server: epoll.wait() {}", e);
+                return Err(e);
+            }
         };
-
-        'handle_connection: loop{
-            let result = match recv_all(&connection){
-                Ok(kvv) => kvv,
-                Err(Errno::ECONNRESET) => {
-                    println!("server: client disconnected");
-                    break 'handle_connection;
-                },
-                Err(e) => {
-                    eprintln!("kv-server: kv-shared::recv_all: error {}", e);
-                    return Err(e);
-                }
-            };
-
-            let result_as_str = String::from_utf8(result.data).unwrap();
-            let mut parts = result_as_str.trim().splitn(3, ' ');
-
-            let command = parts.next().unwrap_or("");
-            let key = parts.next().unwrap_or("");
-            let value = parts.next().unwrap_or("");
-
-            match command {
-                "GET" => {
-                    let msg = String::from("good get!").into_bytes();
-                    send_all(&connection, msg).unwrap();
-                    println!("server: handled GET");
-                },
-                "SET" => {
-                    let msg = String::from("good set!").into_bytes();
-                    send_all(&connection, msg).unwrap();
-                    println!("server: handled SET");
-    
-                },
-                "DEL" => {
-                    let msg = String::from("good del!").into_bytes();
-                    send_all(&connection, msg).unwrap();
-                    println!("server: handled DEL");
-    
-                },
-                _ => {
-                    println!("server: received unknown command {}", result_as_str);
-                }
+        
+        for event in events {
+            if event.data() == PollInterests::ListeningSocket as u64 {
+                /* todo: write connectionfd into a buffer */
+                println!("server: got a {:?} event on Listening Socket", event.events());
+                handle_connection(fds.get(&(PollInterests::ListeningSocket as u64)).expect("get")).unwrap();
+            } else {
+                println!("Got an unhandled {:?} with data {:?}", event.events(), event.data());
             }
         }
 
-        //break 'accept_connections with stdin
     }
 
     close(socket_fd).expect("close socket_fd failed");
